4150278af3e2e32245c8dccf76c1d665fd6b4342
jonathan <jonathan@jeudi8.io>
2022-10-21 15:54:19 +0200

    refactor: for clarity

--- src/scripts/menu.ts
+++ src/scripts/menu.ts
@@ -4,15 +4,7 @@ import selector from './selector';
 
 const MENU = '#menu';
 const MENU_LINK = '.menu-link';
-let display = false;
-
-const tl = gsap
-  .timeline({
-    paused: true,
-    defaults: { duration: 2, opacity: 1, ease: Power4.easeOut },
-  })
-  .to(MENU, {})
-  .to(MENU_LINK, { stagger: 0.1 }, '<50%');
+let displayed = false;
 
 const show = () => {
   const menu = selector(MENU)[0];
@@ -29,16 +21,22 @@ const hide = () => {
   menu.classList.add('hidden');
 };
 
+const tl = gsap
+  .timeline({
+    paused: true,
+    defaults: { duration: 2, opacity: 1, ease: Power4.easeOut },
+  })
+  .to(MENU, {})
+  .to(MENU_LINK, { stagger: 0.1 }, '<50%');
+
+tl.eventCallback('onReverseComplete', hide);
+tl.eventCallback('onStart', show);
+
 const click = () => {
   tl.pause();
-  if (display) {
-    tl.reverse();
-    tl.eventCallback('onReverseComplete', hide);
-  } else {
-    tl.play();
-    tl.eventCallback('onStart', show);
-  }
-  display = !display;
+  if (displayed) tl.reverse();
+  else tl.play();
+  displayed = !displayed;
 };
 
 export const attach = (str: string) => {
